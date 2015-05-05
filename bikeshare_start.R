f7cf553632ccbb79ee6e80f627ed292081dc15cc
Tom Besio <thomas.besio@gmail.com>
2015-05-05 13:09:34 -0500

    Update for Q1

--- bikeshare_start.R
+++ bikeshare_start.R
@@ -6,13 +6,32 @@ biketab[, c("dteday", "mnth","season","weekday","hr","weathersit") := list(
   factor(dteday), factor(mnth), factor(season),
   factor(weekday), factor(hr), factor(weathersit))]
 
-####### Q1: outliers and FDR
-## the next command calculates total cnt by day,
+### Q1: outliers and FDR
+# the next command calculates total cnt by day,
 # also keeping track of the corresponding yr and mnth id.
 daytots <- biketab[, list(total=sum(cnt)), by=c("dteday","yr","mnth")]
 row.names(daytots) <- daytots$dteday
 # simple regression
 daylm <- glm(total ~ yr*mnth, data=daytots)
+
+## 1.1 Show SSE and R2
+# We know that the SSE is known as the 'residual deviance' in R
+# daylm$dev
+# ~> [1] 726316624
+
+# We calculate R2 as 1 - SSE/SST or 1 - residual deviance / null deviance
+daylm.r2 <- 1 - daylm$dev/daylm$null.dev
+# daylm.r2
+# ~> [1] 0.734876
+
+## 1.2 Code to plot the example that I use (EV for February 2012)
+# plot(daytots$total[daytots$yr==1&daytots$mnth==2])
+# abline(h=predict(daylm)["2012-02-01"])
+
+## 1.3
+# First calculate the standardized residuals for daylm
+# The estimate of sigma.hat is sqrt(SSE/(n-2))
+daylm.sigma.hat <- (daylm$dev/())
 
 #### Q2: lasso regression
 library(gamlr)
